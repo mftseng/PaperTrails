22f35bf7d651b231a19ee6f118f95b3d07b64d9f
mftseng <mftseng@calpoly.edu>
2023-10-09 13:53:44 -0700

    episode2

---
+++ main/GamePanel.java
@@ -0,0 +1,45 @@
+package main;
+
+import Inputs.KeyboardInputs;
+import Inputs.MouseInputs;
+
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePanel extends JPanel {
+
+    private MouseInputs mouseInputs;
+    private int xDelta = 100, yDelta = 100;
+    public GamePanel(){
+        mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+
+    }
+
+    public void changeXDelta(int value){
+        this.xDelta += value;
+        repaint();
+    }
+
+    public void changeYDelta(int value){
+        this.yDelta += value;
+        repaint();
+    }
+
+    public void setRectPos(int x, int y){
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.fillRect(xDelta,yDelta,200,50);
+    }
+
+}
--- main/GameWindow.java
+++ main/GameWindow.java
@@ -4,9 +4,14 @@ import javax.swing.*;
 
 public class GameWindow{
     private JFrame jframe;
-    public GameWindow(){
+    public GameWindow(GamePanel gamePanel){
+
         jframe = new JFrame();
+
         jframe.setSize(400,400);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);
+        jframe.setLocationRelativeTo(null);
         jframe.setVisible(true);
 
     }
