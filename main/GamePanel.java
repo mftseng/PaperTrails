5be4e74d31db3dccb96f27432ad5c33ac45cca1b
mftseng <mftseng@calpoly.edu>
2023-10-09 21:50:54 -0700

    rect

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -11,7 +11,10 @@ import java.awt.event.KeyListener;
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 0.01f, yDir = 0.01f;
+    private long lastCheck;
+    private int frames;
     public GamePanel(){
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
@@ -27,19 +30,39 @@ public class GamePanel extends JPanel {
 
     public void changeYDelta(int value){
         this.yDelta += value;
-        repaint();
+
     }
 
     public void setRectPos(int x, int y){
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        g.fillRect(xDelta,yDelta,200,50);
+        updateRectangle();
+        g.fillRect((int)xDelta, (int)yDelta,200,50);
+        frames ++;
+        if (System.currentTimeMillis() - lastCheck >= 1000){
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
+        repaint();
     }
+
+    private void updateRectangle() {
+        xDelta += xDir;
+        if (xDelta > 400 || xDelta < 0){
+            xDir *= -1;
+        }
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta < 0){
+            yDir *=-1;
+        }
+    }
+
 
 }
