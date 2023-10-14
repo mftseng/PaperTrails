b32324351de3b723834dba6d3f64ce7deb07a138
Vince Doan <vincedoanschool@gmail.com>
2023-10-13 20:07:09 -0700

    player_sprite_animations

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -3,9 +3,12 @@ package main;
 import Inputs.KeyboardInputs;
 import Inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class GamePanel extends JPanel {
@@ -17,19 +20,48 @@ public class GamePanel extends JPanel {
     private int frames;
     
     private BufferedImage img;
+    private BufferedImage[][] animations;
+    private int aniTick, aniIndex, aniSpeed = 17;
 
 
     public GamePanel(){
         mouseInputs = new MouseInputs(this);
+
         setPanelSize();
         importImage();
+        loadAnimations();
+
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
 
     }
 
-    private importImage() {
+    private void loadAnimations() {
+        animations = new BufferedImage[7][7];
+
+        for(int i = 0; i < animations.length; i++)
+            for (int j = 0; j < animations[i].length; j++)
+                animations[i][j] = img.getSubimage(j * 160, i * 160, 120, 130);
+
+
+    }
+
+    private void importImage() {
+        InputStream is = getClass().getResourceAsStream("/Stick_figs.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                is.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
     }
 
 
@@ -58,11 +90,24 @@ public class GamePanel extends JPanel {
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        
-        g.drawImage(null, x, y, null);
+
+        updateAnimationTick();
+        g.drawImage(animations[2][aniIndex], (int) xDelta, (int) yDelta, 120, 130, null);
+
 
     
 
+    }
+
+    private void updateAnimationTick() {
+        aniTick++;
+        if(aniTick >= aniSpeed){
+            aniTick = 0;
+            aniIndex++;
+            if (aniIndex >= 6){
+                aniIndex = 0;
+            }
+        }
     }
 
 
