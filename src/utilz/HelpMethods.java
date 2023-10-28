e224b4ec051470787ab65be7de093683dc85d8c2
Vince Doan <vincedoanschool@gmail.com>
2023-10-27 23:09:28 -0700

    gravity_2

--- src/utilz/HelpMethods.java
+++ src/utilz/HelpMethods.java
@@ -10,27 +10,24 @@ public class HelpMethods {
     private static boolean rightOverlap;
     private static boolean bottomOverlap;
     private static boolean leftOverlap;
-
     public static boolean CanMoveHere(float x, float y, int width, int height, Rectangle[] lvlDat) {
-        // Boundaries of the game
-        if (x < 0 || x + width > Game.GAME_WIDTH || y < 0 || y > Game.GAME_HEIGHT)
+        //Boundaries of the game
+        if (x < 0 || x + width > Game.GAME_WIDTH)
             return false;
-
-
-        // Check for collision with level objects
+        if (y < 0 || y > Game.GAME_HEIGHT)
+            return false;
+        //Checks for collision of level objects
         for (int i = 0; i < lvlDat.length; i++) {
-            leftOverlap = (x + width > lvlDat[i].x);
-            rightOverlap = (x < lvlDat[i].x + lvlDat[i].width);
-            bottomOverlap = (y + height > lvlDat[i].y);
-            topOverlap = (y < lvlDat[i].y + lvlDat[i].height);
-
-            if (leftOverlap && rightOverlap && bottomOverlap && topOverlap) {
+            if ((lvlDat[i].x + lvlDat[i].width >= (int) x) &&
+                    (lvlDat[i].y + lvlDat[i].height >= (int) y) &&
+                    ((int) x + width >= lvlDat[i].x) &&
+                    ((int) y + height >= lvlDat[i].y)) {
                 return false; // Collision detected
             }
         }
-
         return true; // No collision detected
     }
+
 
     public static boolean getTopOverlap(){
         return topOverlap;
